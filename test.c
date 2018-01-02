a532c256d5e393f80f157eed335594918ae69f1c
thanhcong113 <thanhcongjapan@gmail.com>
2018-01-03 00:48:35 +0900

    c
    thu c

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a=0,b=0,c=0;
+printf("\tnhap_so_a:\n\a");
+scanf("%d",&a);
+printf("\tnhap_so_b:\n\a");
+scanf("%d",&b);
+printf("\t so thu nhat la %d, so thu hai la %d\n\a",a,b);
+c=a+b;
+printf("\t Tong_hai_so_la_%d\n\a",c);
+return 0;
+}
